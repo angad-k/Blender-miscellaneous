035214a5b3f715c43a2644c87cfdc74c1f097ed1
Angad Kambli <angadkambli@gmail.com>
2021-04-11 00:05:35 +0530

    add loading screen fade out

--- script.js
+++ script.js
@@ -8,12 +8,22 @@
         else
         img.addEventListener( 'load', incrementCounter, false );
     } );
+    var opacity = 1;
+    function lto() {
+        var box = document.getElementById('loading-banner');
+        opacity -= 0.05;
+        if (opacity < 0)
+            {document.getElementById("loading-banner").remove(); return;}
+        box.style.opacity = opacity;
+        requestAnimationFrame(lto);
+    }
 
     function incrementCounter() {
         counter++;
         document.getElementById("loader").style.width = '' + counter/len*100 + '%'
-        if ( counter === len ) {
-            document.getElementById("loading-banner").remove()
+        if ( counter === len ) {           
+            lto()
         }
     }
 
+    
